2726c7660dbc9c0eb95305c598a7d60ff518fc75
Colombianito66 <colombianito66@gmail.com>
2018-07-07 10:44:17 -0400

    practice with forms

--- client/src/Form.js
+++ client/src/Form.js
@@ -2,15 +2,35 @@ import React, { Component} from 'react';
 
 
 class Form extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  }
+
+
   render() {
     return (
       <div>
         <h1>M-Lab Database</h1>
         <form>
-          <input 
-            placeholder="What is your email?"
+          <input
+            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={event => this.setState({firstName: event.target.value})}
           />
-          <button> SUBMIT </button>
+          <input 
+            placeholder="Last Name"
+            value={this.state.lastName}
+            onChange={event => this.setState({lastName: event.target.value})}
+          />
+          <input 
+            placeholder="E-Mail Address"
+            value={this.state.email}
+            onChange={event => this.setState({email: event.target.value})}
+          />
+         
+          <button onClick={(e) => this.onSubmit(e)}> SUBMIT </button>
         </form>
       </div>
     )
