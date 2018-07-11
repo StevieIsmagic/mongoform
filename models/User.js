92d7178b80114129974fcaaab43af07eb1eccc29
Colombianito66 <colombianito66@gmail.com>
2018-07-10 22:47:21 -0400

    update schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,13 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String
+  email: String,
+  phone: String,
+  comments: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('users', userSchema);
